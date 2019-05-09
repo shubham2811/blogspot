2c325f9a2cd7adc26a3aae7823f62af63b5759c7
Shubham Sharma <Shubham.Sharma@telusinternational.com>
2019-05-09 07:26:44 +0530

    build

---
+++ build1/precache-manifest.5f29f3aae4afe8423ed86903c2479233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d04542df4e7a29c6fe3b77f633f209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452f9425df14d3f02268",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "07f8c43a98a39d87c7dd",
+    "url": "/static/js/2.57080b02.chunk.js"
+  },
+  {
+    "revision": "452f9425df14d3f02268",
+    "url": "/static/js/main.fe0017df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
